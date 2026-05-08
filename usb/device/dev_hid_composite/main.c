08a5f9cb3759ae50239dc540f477d6f31fff5946
hellowangxifeng <71929404+hellowangxifeng@users.noreply.github.com>
2026-05-08 13:29:05 +0800

    Update main.c

--- usb/device/dev_hid_composite/main.c
+++ usb/device/dev_hid_composite/main.c
@@ -218,3 +218,38 @@ int main(void)
 
     return 0;
 }
+
+uint16_t tud_hid_get_report_cb(
+    uint8_t instance,
+    uint8_t report_id,
+    hid_report_type_t report_type,
+    uint8_t* buffer,
+    uint16_t reqlen
+)
+{
+    (void) instance;
+    (void) report_id;
+    (void) report_type;
+    (void) buffer;
+    (void) reqlen;
+
+    // We do not use GET_REPORT in this project.
+    return 0;
+}
+
+void tud_hid_set_report_cb(
+    uint8_t instance,
+    uint8_t report_id,
+    hid_report_type_t report_type,
+    uint8_t const* buffer,
+    uint16_t bufsize
+)
+{
+    (void) instance;
+    (void) report_id;
+    (void) report_type;
+    (void) buffer;
+    (void) bufsize;
+
+    // We do not use SET_REPORT in this project.
+}
