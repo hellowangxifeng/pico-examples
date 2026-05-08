28b2fb965a048e571e13e309dec3a66a400d5afb
hellowangxifeng <71929404+hellowangxifeng@users.noreply.github.com>
2026-05-08 13:13:35 +0800

    Update main.c

--- usb/device/dev_hid_composite/main.c
+++ usb/device/dev_hid_composite/main.c
@@ -1,44 +1,13 @@
 /*
- * The MIT License (MIT)
- *
- * Copyright (c) 2019 Ha Thach (tinyusb.org)
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- *
+ * RP2040 CDC Serial + HID Keyboard + Mouse Controller
  */
 
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <stdint.h>
 
 #include "bsp/board_api.h"
-#include "tusb.h"
-
-#include "usb_descriptors.h"
-
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include <ctype.h>
-
-#include "pico/stdlib.h"
-#include "bsp/board.h"
 #include "tusb.h"
 #include "usb_descriptors.h"
 
@@ -172,6 +141,7 @@ static void handle_command(char* line)
     if (!strcmp(cmd, "MOVE")) {
         char* sx = strtok(NULL, " \r\n");
         char* sy = strtok(NULL, " \r\n");
+
         if (sx && sy) {
             send_mouse_move(atoi(sx), atoi(sy));
         }
@@ -189,18 +159,21 @@ static void handle_command(char* line)
     }
     else if (!strcmp(cmd, "KEY")) {
         char* keyname = strtok(NULL, " \r\n");
+
         if (keyname) {
             send_key(keycode_from_name(keyname));
         }
     }
     else if (!strcmp(cmd, "SCROLL")) {
         char* amount = strtok(NULL, " \r\n");
+
         if (amount) {
             send_scroll(atoi(amount));
         }
     }
     else if (!strcmp(cmd, "TEXT")) {
         char* text = strtok(NULL, "\r\n");
+
         if (text) {
             type_text(text);
         }
@@ -211,7 +184,6 @@ int main(void)
 {
     board_init();
 
-    // init TinyUSB device stack
     tud_init(BOARD_TUD_RHPORT);
 
     if (board_init_after_tusb) {
@@ -237,7 +209,7 @@ int main(void)
 
                 pos = 0;
             } else {
-                if (pos < sizeof(line) - 1) {
+                if (pos < (int)sizeof(line) - 1) {
                     line[pos++] = c;
                 }
             }
@@ -245,5 +217,4 @@ int main(void)
     }
 
     return 0;
-}
 }
